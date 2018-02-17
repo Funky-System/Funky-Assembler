05b0b9c16ec15aecea633aae2a0739e5772eae0f
Bas de Ruijter <Bas@Funky-System.org>
2018-02-17 11:22:59 +0100

    guard

--- include/funkyas/funkyas.h
+++ include/funkyas/funkyas.h
@@ -8,11 +8,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#ifndef FUNKY_BYTECODE_TYPES_DEFINED
+#define FUNKY_BYTECODE_TYPES_DEFINED
 typedef unsigned char byte_t;
-typedef struct {
+typedef struct funky_bytecode_t {
     byte_t* bytes;
     unsigned long length;
 } funky_bytecode_t;
+#endif
 
 int funky_assemble_files(const char *filename, const char *filename_output, int strip_debug);
 funky_bytecode_t funky_assemble(const char *filename_hint, const char *input, int strip_debug);
